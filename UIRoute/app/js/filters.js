a84430062d9a0e0bea03eed04f0408f3742f12cc
wangbin <bwang2009@yeah.net>
2015-10-13 23:42:09 +0800

    # UI Route
    Signed-off-by: wangbin <bwang2009@yeah.net>

---
+++ UIRoute/app/js/filters.js
@@ -0,0 +1,9 @@
+var bookStoreFilters = angular.module('bookStoreFilters', []);
+
+bookStoreDirectives.filter('bookStoreFilter_1', ['$scope',
+    function($scope) {}
+]);
+
+bookStoreDirectives.filter('bookStoreFilter_2', ['$scope',
+    function($scope) {}
+]);
---
+++ UIRoute/app/js/services.js
@@ -0,0 +1,9 @@
+var bookStoreServices = angular.module('bookStoreServices', []);
+
+bookStoreDirectives.service('bookStoreService_1', ['$scope',
+    function($scope) {}
+]);
+
+bookStoreDirectives.service('bookStoreService_2', ['$scope',
+    function($scope) {}
+]);
